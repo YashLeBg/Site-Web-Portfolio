4b031bc6ceeb19c280c8a2738ea5b0f9688313c2
yash <yash.mohamed976@gmail.com>
2024-06-11 20:10:09 +0200

    Fin(accueil.component)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  animations: [
+    trigger("slideInOut", [
+      state(
+        "in",
+        style({
+          maxHeight: "500px",
+          opacity: 1,
+        })
+      ),
+      transition(":enter", [
+        style({ maxHeight: "0px", opacity: 0 }),
+        animate("0.5s ease-out"),
+      ]),
+      transition(":leave", [
+        animate("0.5s ease-out", style({ maxHeight: "0px", opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'site-cv';
+  title = "site-cv";
 }
