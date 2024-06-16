4fc1906c49ac1c09933ade48f3791bcc6f0e5371
yash <yash.mohamed976@gmail.com>
2024-06-16 17:53:20 +0200

    feat(projets.component) adding projects

--- src/app/projets/projets.component.ts
+++ src/app/projets/projets.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-projets',
-  templateUrl: './projets.component.html',
-  styleUrls: ['./projets.component.css']
+  selector: "app-projets",
+  templateUrl: "./projets.component.html",
+  styleUrls: ["./projets.component.css"],
 })
-export class ProjetsComponent {
-
-}
+export class ProjetsComponent {}
