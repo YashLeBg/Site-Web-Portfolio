8db1ac9e700c76cf34ef9dcb1f5b78a36b6713a6
yash <yash.mohamed976@gmail.com>
2025-03-04 03:19:35 +0100

    add(Competences in projects)

--- src/app/projets/projets.component.ts
+++ src/app/projets/projets.component.ts
@@ -9,30 +9,57 @@ export class ProjetsComponent {
   isModal1Open = true;
   isModal2Open = true;
   isModal3Open = true;
+  isModal4Open = true;
+  isModal5Open = true;
+  isModal6Open = true;
+  isModal7Open = true;
+
+  realiser = "#B80805";
+  optimiser = "#ED8C51";
+  administrer = "#EFBA12";
+  gerer = "#8CC750";
+  conduire = "#0B205D";
+  collaborer = "#0F0F0F";
 
   boutonsModal1 = [
-    { label: "Réaliser", couleurTexte: "#FF5733" },
-    { label: "Optimiser", couleurTexte: "#4CAF50" },
-    { label: "Optimiser", couleurTexte: "#4CAF50" },
-    { label: "Optimiser", couleurTexte: "#4CAF50" },
-    { label: "Optimiser", couleurTexte: "#4CAF50" },
+    { label: "Réaliser", couleurTexte: this.realiser },
+    { label: "Conduire", couleurTexte: this.conduire },
+    { label: "Optimiser", couleurTexte: this.optimiser },
   ];
 
   boutonsModal2 = [
-    { label: "Compétence X", couleurTexte: "#2196F3" },
-    { label: "Compétence Y", couleurTexte: "#FFC107" },
-    { label: "Compétence Z", couleurTexte: "#E91E63" },
-    { label: "Compétence Z", couleurTexte: "#E91E63" },
-    { label: "Compétence Z", couleurTexte: "#E91E63" },
-    { label: "Compétence Z", couleurTexte: "#E91E63" },
-    { label: "Compétence Z", couleurTexte: "#E91E63" },
-    { label: "Compétence Z", couleurTexte: "#E91E63" },
+    { label: "Réaliser", couleurTexte: this.realiser },
+    { label: "Conduire", couleurTexte: this.conduire },
+    { label: "Collaborer", couleurTexte: this.collaborer },
+    { label: "Optimiser", couleurTexte: this.optimiser },
   ];
 
   boutonsModal3 = [
-    { label: "Technologie X", couleurTexte: "#2196F3" },
-    { label: "Technologie Y", couleurTexte: "#FFC107" },
-    { label: "Technologie Z", couleurTexte: "#E91E63" },
+    { label: "Réaliser", couleurTexte: this.realiser },
+    { label: "Conduire", couleurTexte: this.conduire },
   ];
 
+  boutonsModal4 = [
+    { label: "Réaliser", couleurTexte: this.realiser },
+    { label: "Conduire", couleurTexte: this.conduire },
+    { label: "Collaborer", couleurTexte: this.collaborer },
+    { label: "Gérer", couleurTexte: this.gerer },
+    { label: "Administrer", couleurTexte: this.administrer },
+  ];
+
+  boutonsModal5 = [
+    { label: "Réaliser", couleurTexte: this.realiser },
+    { label: "Conduire", couleurTexte: this.conduire },
+  ];
+
+  boutonsModal6 = [
+    { label: "Réaliser", couleurTexte: this.realiser },
+    { label: "Conduire", couleurTexte: this.conduire },
+  ];
+
+  boutonsModal7 = [
+    { label: "Réaliser", couleurTexte: this.realiser },
+    { label: "Conduire", couleurTexte: this.conduire },
+    { label: "Collaborer", couleurTexte: this.collaborer },
+  ];
 }
